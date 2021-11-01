1151ec714911d9cf0f201d983c6bc2a8b23a7c1a
Eason Chen <easoncxz@gmail.com>
2021-11-02 11:45:20 +1300

    Wire up Servant server into Wai/Hspec scaffolding (tests red)

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -10,12 +10,17 @@ module MyLib
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
 
+import CounterStorage (newCounter)
+import HttpServer (counterApplication)
+
 -- TODO: You should implement this function. See the separately provided
 -- description on what the application should do.
 --
 -- Hint, scotty, servant and yesod are libraries that can provide the 'Application'.
 application :: IO Application
-application = error "implement me"
+application = do
+  counter <- newCounter
+  return (counterApplication counter)
 
 defaultMain :: IO ()
 defaultMain = run 8080 =<< application
