c8454f64a0aced0ba9997e7e21a7a3459004bea3
Eason Chen <easoncxz@gmail.com>
2021-11-02 13:10:07 +1300

    Include `Cache-Control: no-store`

--- src/HttpServer.hs
+++ src/HttpServer.hs
@@ -1,22 +1,35 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module HttpServer
   ( counterApplication
   ) where
 
 import Data.Proxy (Proxy(Proxy))
+import Data.Text (Text)
 import Network.Wai (Application)
-import Servant.API ((:<|>)((:<|>)), (:>), Get, JSON, Post)
+import Servant.API
+  ( (:<|>)((:<|>))
+  , (:>)
+  , Get
+  , Header
+  , Headers
+  , JSON
+  , Post
+  , addHeader
+  )
 import Servant.Server (Server, serve)
 
 import CounterStorage (Counter)
 import qualified CounterStorage as Storage
 import HttpEntities (CounterResponse(CounterResponse))
 
+type CacheControlHeader = Header "Cache-Control" Text
+
 -- Apologies, I only have hindent set up, which formats Servant's type like this.
 type CounterApi
-   = "requests" :> Get '[ JSON] CounterResponse :<|> "requests" :> "reset" :> Post '[ JSON] CounterResponse
+   = "requests" :> Get '[ JSON] (Headers '[ CacheControlHeader] CounterResponse) :<|> "requests" :> "reset" :> Post '[ JSON] CounterResponse
 
 counterServer :: Counter -> Server CounterApi
 counterServer counter = handleIncrement :<|> handleReset
@@ -28,7 +41,7 @@ counterServer counter = handleIncrement :<|> handleReset
      = do
       current <- Storage.peek counter
       _ <- Storage.increment counter
-      return (CounterResponse current)
+      return $ addHeader ("no-store" :: Text) (CounterResponse current)
     handleReset = do
       zero <- Storage.reset counter
       return (CounterResponse zero)
